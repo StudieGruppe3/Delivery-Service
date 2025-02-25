526c23979222e14045a0d77755fab6997460bb84
nico132a <eaa23nive@students.eaaa.dk>
2025-02-25 13:26:28 +0100

    initial commit

---
+++ deliveryServiceAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace deliveryServiceAPI.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
